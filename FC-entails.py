b4f9be6a25a3804a57fb613a5769ef4946cdef9b
Giulia <giulia.paone@stud.unifi.it>
2022-03-30 23:00:49 +0200

    algorithms and first example with inference in wumpus world

--- FC-entails.py
+++ FC-entails.py
@@ -38,10 +38,11 @@ class KnowledgeBase:
             self.listOfRules.append(rule)
 
 
-def FCEntailment(kb, q):
+def FCEntails(kb, q):
     count = {}
     inferred = {}
     queue = []
+    order = []
 
     for symbol in kb.listOfSymbols:
         inferred[symbol.lit] = False
@@ -50,11 +51,12 @@ def FCEntailment(kb, q):
         count[rule.name] = rule.numOfSymbols
 
     for symbol in kb.listOfSymbols:
-        if hasattr(symbol, 'truthValue') and symbol.truthValue == True:
+        if hasattr(symbol, 'truthValue') and symbol.truthValue :
             queue.append(symbol)
 
     while len(queue) != 0:
         p = queue.pop()
+        print(p.lit)
         if p == q:
             return True
         if not inferred[p.lit]:
@@ -70,7 +72,8 @@ def FCEntailment(kb, q):
     return False
 
 
-def BCEntailment(kb, q):
+
+def BCEntailment(kb, q, orderList=[]):
     count = {}
     inferred = {}
 
@@ -80,28 +83,28 @@ def BCEntailment(kb, q):
     for rule in kb.listOfRules:
         count[rule.name] = rule.numOfSymbols
 
-    BCInference(kb, q, inferred, count)
+    return BCEntails(kb, q, inferred, count, orderList)
 
-    if hasattr(q, 'truthValue') and q.truthValue:
+
+def BCEntails(kb, q, inferred=[], count=[], orderList=[]):
+    if inferred[q.lit]:
         return True
-    else:
-        return False
-
-
-def BCInference(kb, q, inferred, count):
-    for rule in kb.listOfRules:
-        for c in rule.consequences:
+    print(q.lit)
+    if hasattr(q, 'truthValue') and q.truthValue:
+        orderList.append(q.lit)
+        return True
+    for r in kb.listOfRules:
+        for c in r.consequences:
             if q == c:
-                for p in rule.premises:
-                    if not inferred[p.lit]:
-                        if hasattr(p, 'truthValue') and p.truthValue:
-                            inferred[p.lit] = True
-                            count[rule.name] -= 1
-                    else:
-                        BCInference(kb, p, inferred, count)
-            if count[rule.name] == 0:
-                q.truthValue = True
-                inferred[q.lit] = True
+                sum = 0
+                for p in r.premises:
+                    result = BCEntails(kb, p, inferred, count, orderlist)
+                    if result:
+                        sum=sum+1
+                if sum == r.numOfSymbols:
+                    q.truthValue = True
+                    return True
+    return False
 
 
 nB11 = Literal("B(1,1)", True)
@@ -110,18 +113,25 @@ nB11.setTruthValue(True)
 #B21.setTruthValue(True)
 nP12 = Literal("P(1,2)", True)
 nP21 = Literal("P(2,1)", True)
-P11 = Literal("P(1,1)")
+nP12 = Literal("P(1,2)", True)
 
 
-R1 = Sentence([nB11], [nP12, nP21], "R1")
+R1 = Sentence([nB11], [nP12], "R1")
+R2 = Sentence([nB11], [nP21], "R2")
 
 
-symbols = [nB11, nP12, nP21, P11]
-rules = [R1]
+
+symbols = [nB11, nP12, nP21, nP12]
+rules = [R1, R2]
 
 KB = KnowledgeBase(symbols, rules)
-result = BCEntailment(KB, P11)
-
+result = FCEntails(KB, nP12)
 print(result)
+
+print("bc:")
+orderlist = []
+result2 = BCEntailment(KB, nP12, orderlist)
+print(result2)
+print(orderlist)
 
 
